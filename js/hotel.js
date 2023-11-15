134f5c60b02dd6832c84199f12d068efde2c824a
autumnus01 <autumn2hurley2@gmail.com>
2023-11-15 12:48:27 -0500

    in class

--- js/hotel.js
+++ js/hotel.js
@@ -14,3 +14,41 @@ console.log(test.hotelname);
 console.log(test.booked);
 test.booked = 144;
 console.log(test.booked);
+
+//Start the hotel reservation
+var guestgroup = [];
+function reservation() {
+    //Get the values from the form, and put it in an object
+
+    // Practice -- complete object
+    var guest = {
+        name: $('#name').val(),
+        numberofGuest: $('#num').val(),
+        checkIn: $('#checkin').val(),
+        checkOut: $('#checkout').val(),
+        roomType: $('#room').val()
+
+    };
+
+    guestgroup.push(guest);
+    console.log(guest);
+    alert("Thank you for your reservation");
+}
+function displayList(){
+    $('#guestlist').html("");
+    $('#guestlist').append("<table>");
+    for(var i in guestgroup){
+        $('#guestlist').append("<tr><td>" + guestgroup[i].name + "</td>");
+        $('#guestlist').append("<td>" + guestgroup[i].numberofGuest + "</td>");
+        $('#guestlist').append("<td>" + guestgroup[i].checkIn + "</td>");
+        $('#guestlist').append("<td>" + guestgroup[i].checkOut + "</td>");
+        $('#guestlist').append("<td>" + guestgroup[i].roomType + "</td></tr>");
+        
+    }
+    $('#guestlist').append("</table>");
+}
+
+
+// Register the event handler
+$('#submit').click(reservation);
+$('#lists').click(displayList);
